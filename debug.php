ff022241d3ff0b48d9548ffd673cba5e35c6e270
Tom Ransom <transom@1bigidea.com>
2014-03-03 12:35:18 -0800

    Use WP_CONTENT_DIR instead of ABSPATH, Add new debugging for doing_it_wrong (backtrace)

--- debug.php
+++ debug.php
@@ -15,7 +15,7 @@ if( $debug && !class_exists('ChromePhp') ) require_once('chromephp/ChromePhp.php
 if( !class_exists('trrDebugClass') ){
 	class trrDebugClass{
 		var $debug_stack = array();
-		
+
 		function __construct(){
 			if( defined('TRR_DEBUG') && TRR_DEBUG ){
 				add_action( 'wp_foot', array($this, 'trr_debug_dump') );
@@ -23,51 +23,60 @@ if( !class_exists('trrDebugClass') ){
 				add_action( 'all', array($this, 'trr_debug_hooks'));
 				add_action( 'wp_enqueue_scripts', array($this, 'trr_debug_jq_dump'));
 				add_action('admin_enqueue_scripts', array($this, 'trr_debug_jq_dump'));
+
+				add_action('doing_it_wrong_run', array($this, 'doing_it_wrong_backtrace'), 10, 3 );
 			}
 			if( defined('SAVEQUERIES') && SAVEQUERIES){
-				add_action('wp_foot', array($this, 'trr_debug_queries'));				
-				add_action('admin_footer', array($this, 'trr_debug_queries'));				
+				add_action('wp_foot', array($this, 'trr_debug_queries'));
+				add_action('admin_footer', array($this, 'trr_debug_queries'));
 			}
 		}
 		function trr_debug_dump() {
 			global $bb_post, $topic, $wp_actions, $bb_views;
 			global $bb_current_user, $profile_menu;
-			
+
 			krumo($this->debug_stack);
 			krumo($profile_menu);
-			
-			return;	
-		}	
+
+			return;
+		}
 		function trr_debug_hooks() {
 			global $debug_stack_trr;
-			
+
 			$debug_stack_trr[] = current_filter();
-			
+
 			return;
 		}
 		function trr_debug_jq_dump() {
 			wp_enqueue_script('jquery');
-			wp_enqueue_script('jq_dump', plugins_url(basename(dirname(__FILE__)).'/jquery.dump.js'),array('jquery'));			
+			wp_enqueue_script('jq_dump', plugins_url(basename(dirname(__FILE__)).'/jquery.dump.js'),array('jquery'));
 		}
 		function trr_debug_queries () {
 			global $wpdb;
-			
+
 			krumo($wpdb->queries);
 		}
-		function backTrace() 
-		{ 
-			$bt=debug_backtrace(); 
-			$sp=0; 
-			$trace=""; 
-			foreach($bt as $k=>$v) 
-			{ 
-				extract($v); 
-				$file=substr($file,1+strrpos($file,"/")); 
-				if($file=="db.php")continue; // the db object 
-				$trace.=str_repeat("&nbsp;",++$sp); //spaces(++$sp); 
-				$trace.="file=$file, line=$line, function=$function<br>";        
-			} 
-			kickout('backtrace','Backtrace from '.$_SERVER['HTTP_HOST'], $trace); 
+
+		function get_backTrace() {
+			$bt=debug_backtrace();
+			$sp=0;
+			$trace="";
+			foreach($bt as $k=>$v)
+			{
+				extract($v);
+				$file=substr($file,1+strrpos($file,"/"));
+				if($file=="db.php")continue; // the db object
+				$trace.=str_repeat("&nbsp;",++$sp); //spaces(++$sp);
+				$trace.="file=$file, line=$line, function=$function<br>";
+			}
+		}
+
+		function backTrace() {
+			kickout('backtrace','Backtrace from '.$_SERVER['HTTP_HOST'], $this->get_backTrace() );
+		}
+
+		function doing_it_wrong_backtrace($function, $message, $version) {
+			kickout('doing_it_wrong_'.$function, wp_debug_backtrace_summary(), FILE_APPEND);
 		}
 	}
 }
@@ -75,11 +84,11 @@ if( $debug )
 	new trrDebugClass;
 if( !function_exists('kickout') ){
 	function kickout(){
-		
+
 		$args = func_get_args();
-		
+
 		$file_output = array_shift($args);
-		
+
 		$file_append = false;
 
 		// prepare the output
@@ -94,7 +103,7 @@ if( !function_exists('kickout') ){
 			}
 		}
 
-		file_put_contents(ABSPATH.'/wp-content/_'.$file_output.'.txt', $output, ($file_append) ? FILE_APPEND : null);	
+		file_put_contents(WP_CONTENT_DIR.'/_'.$file_output.'.txt', $output, ($file_append) ? FILE_APPEND : null);
 
 	}
 }
