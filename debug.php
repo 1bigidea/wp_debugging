14a2a9a380d43d83a7d74eba7285da320d79b3b9
Tom Ransom <transom@1bigidea.com>
2024-02-13 08:58:00 -0500

    Add kickout_html function

--- debug.php
+++ debug.php
@@ -9,113 +9,148 @@ Author URI: http://1bigidea.com
 
 $debug = ((defined('TRR_DEBUG') && TRR_DEBUG) || (defined('WP_DEBUG') && WP_DEBUG));
 
-if( $debug && !class_exists('krumo') ) require_once ('krumo/class.krumo.php');
-if( $debug && !class_exists('ChromePhp') ) require_once('chromephp/ChromePhp.php');
+if ($debug && !class_exists('krumo')) require_once('krumo/class.krumo.php');
+if ($debug && !class_exists('ChromePhp')) require_once('chromephp/ChromePhp.php');
 
-if( !class_exists('trrDebugClass') ){
-	class trrDebugClass{
-		var $debug_stack = array();
+if (!class_exists('trrDebugClass')) {
+    class trrDebugClass
+    {
+        var $debug_stack = array();
 
-		function __construct(){
-			if( defined('TRR_DEBUG') && TRR_DEBUG ){
-				add_action( 'wp_foot', array($this, 'trr_debug_dump') );
-				add_action( 'admin_footer', array($this, 'trr_debug_dump') );
-				add_action( 'all', array($this, 'trr_debug_hooks'));
-				add_action( 'wp_enqueue_scripts', array($this, 'trr_debug_jq_dump'));
-				add_action('admin_enqueue_scripts', array($this, 'trr_debug_jq_dump'));
+        function __construct()
+        {
+            if (defined('TRR_DEBUG') && TRR_DEBUG) {
+                add_action('wp_foot', array($this, 'trr_debug_dump'));
+                add_action('admin_footer', array($this, 'trr_debug_dump'));
+                add_action('all', array($this, 'trr_debug_hooks'));
+                add_action('wp_enqueue_scripts', array($this, 'trr_debug_jq_dump'));
+                add_action('admin_enqueue_scripts', array($this, 'trr_debug_jq_dump'));
 
-				add_action('doing_it_wrong_run', array($this, 'doing_it_wrong_backtrace'), 10, 3 );
-			}
-			if( defined('SAVEQUERIES') && SAVEQUERIES){
-				add_action('wp_foot', array($this, 'trr_debug_queries'));
-				add_action('admin_footer', array($this, 'trr_debug_queries'));
-			}
-		}
-		function trr_debug_dump() {
-			global $bb_post, $topic, $wp_actions, $bb_views;
-			global $bb_current_user, $profile_menu;
+                add_action('doing_it_wrong_run', array($this, 'doing_it_wrong_backtrace'), 10, 3);
+            }
+            if (defined('SAVEQUERIES') && SAVEQUERIES) {
+                add_action('wp_foot', array($this, 'trr_debug_queries'));
+                add_action('admin_footer', array($this, 'trr_debug_queries'));
+            }
+        }
+        function trr_debug_dump()
+        {
+            global $bb_post, $topic, $wp_actions, $bb_views;
+            global $bb_current_user, $profile_menu;
 
-			krumo($this->debug_stack);
-			krumo($profile_menu);
+            krumo($this->debug_stack);
+            krumo($profile_menu);
 
-			return;
-		}
-		function trr_debug_hooks() {
-			global $debug_stack_trr;
+            return;
+        }
+        function trr_debug_hooks()
+        {
+            global $debug_stack_trr;
 
-			$debug_stack_trr[] = current_filter();
+            $debug_stack_trr[] = current_filter();
 
-			return;
-		}
-		function trr_debug_jq_dump() {
-			wp_enqueue_script('jquery');
-			wp_enqueue_script('jq_dump', plugins_url(basename(dirname(__FILE__)).'/jquery.dump.js'),array('jquery'));
-		}
-		function trr_debug_queries () {
-			global $wpdb;
+            return;
+        }
+        function trr_debug_jq_dump()
+        {
+            wp_enqueue_script('jquery');
+            wp_enqueue_script('jq_dump', plugins_url(basename(dirname(__FILE__)) . '/jquery.dump.js'), array('jquery'));
+        }
+        function trr_debug_queries()
+        {
+            global $wpdb;
 
-			krumo($wpdb->queries);
-		}
+            krumo($wpdb->queries);
+        }
 
-		function get_backTrace() {
-			$bt=debug_backtrace();
-			$sp=0;
-			$trace="";
-			foreach($bt as $k=>$v)
-			{
-				extract($v);
-				$file=substr($file,1+strrpos($file,"/"));
-				if($file=="db.php")continue; // the db object
-				$trace.=str_repeat("&nbsp;",++$sp); //spaces(++$sp);
-				$trace.="file=$file, line=$line, function=$function<br>";
-			}
-		}
+        function get_backTrace()
+        {
+            $bt = debug_backtrace();
+            $sp = 0;
+            $trace = "";
+            foreach ($bt as $k => $v) {
+                extract($v);
+                $file = substr($file, 1 + strrpos($file, "/"));
+                if ($file == "db.php") continue; // the db object
+                $trace .= str_repeat("&nbsp;", ++$sp); //spaces(++$sp);
+                $trace .= "file=$file, line=$line, function=$function<br>";
+            }
+        }
 
-		function backTrace() {
-			kickout('backtrace','Backtrace from '.$_SERVER['HTTP_HOST'], $this->get_backTrace() );
-		}
+        function backTrace()
+        {
+            kickout('backtrace', 'Backtrace from ' . $_SERVER['HTTP_HOST'], $this->get_backTrace());
+        }
 
-		function doing_it_wrong_backtrace($function, $message, $version) {
-			kickout('doing_it_wrong_'.$function, wp_debug_backtrace_summary(), FILE_APPEND);
-		}
-	}
+        function doing_it_wrong_backtrace($function, $message, $version)
+        {
+            kickout('doing_it_wrong_' . $function, wp_debug_backtrace_summary(), FILE_APPEND);
+        }
+    }
 }
-if( $debug )
-	new trrDebugClass;
-if( !function_exists('kickout') ){
-	function kickout(){
+if ($debug)
+    new trrDebugClass;
+if (!function_exists('kickout')) {
+    function kickout()
+    {
 
-		$args = func_get_args();
+        $args = func_get_args();
 
-		$file_output = array_shift($args);
+        $file_output = array_shift($args);
 
-		$file_append = false;
+        $file_append = false;
 
-		// prepare the output
-		$output = '';
-		for($i=0;$i<count($args);$i++){
-			if( is_object($args[$i]) || is_array($args[$i]) ){
-				$output .= print_r($args[$i], true)."\n";
-			} elseif( $args[$i] == FILE_APPEND ) {
-				$file_append = true;
-			} else {
-				$output .= "\n".$args[$i]."\n";
-			}
-		}
+        // prepare the output
+        $output = '';
+        for ($i = 0; $i < count($args); $i++) {
+            if (is_object($args[$i]) || is_array($args[$i])) {
+                $output .= print_r($args[$i], true) . "\n";
+            } elseif ($args[$i] == FILE_APPEND) {
+                $file_append = true;
+            } else {
+                $output .= "\n" . $args[$i] . "\n";
+            }
+        }
 
-		file_put_contents(WP_CONTENT_DIR.'/_'.$file_output.'.txt', $output, ($file_append) ? FILE_APPEND : null);
-
-	}
+        file_put_contents(WP_CONTENT_DIR . '/_' . $file_output . '.txt', $output, ($file_append ? FILE_APPEND : 0));
+    }
 }
-if( !function_exists('whereCalled') ){
-	function whereCalled( $level = 1 ) {
-	    $trace = debug_backtrace();
-	    $file   = $trace[$level]['file'];
-	    $line   = $trace[$level]['line'];
-	    $object = $trace[$level]['object'];
-	    if (is_object($object)) { $object = get_class($object); }
+if (!function_exists('kickout_html')) {
+    function kickout_html()
+    {
 
-	    return "Where called: line $line of $object \n(in $file)";
-	}
+        $args = func_get_args();
+
+        $file_output = array_shift($args);
+
+        $file_append = false;
+
+        // prepare the output
+        $output = '';
+        for ($i = 0; $i < count($args); $i++) {
+            if (is_object($args[$i]) || is_array($args[$i])) {
+                $output .= print_r($args[$i], true) . "\n";
+            } elseif ($args[$i] == FILE_APPEND) {
+                $file_append = true;
+            } else {
+                $output .= "\n" . $args[$i] . "\n";
+            }
+        }
+
+        file_put_contents(WP_CONTENT_DIR . '/_' . $file_output . '.html', $output, ($file_append ? FILE_APPEND : 0));
+    }
 }
+if (!function_exists('whereCalled')) {
+    function whereCalled($level = 1)
+    {
+        $trace = debug_backtrace($level);
+        $file   = $trace[$level]['file'];
+        $line   = $trace[$level]['line'];
+        $object = $trace[$level]['object'];
+        if (is_object($object)) {
+            $object = get_class($object);
+        }
 
+        return "Where called: line $line of $object \n(in $file)";
+    }
+}
